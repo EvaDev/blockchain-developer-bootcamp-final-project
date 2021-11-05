2375187d7d8f8845acadd196bf66a48dfda48410
EvaDev <48243502+EvaDev@users.noreply.github.com>
2021-11-05 16:00:37 +0200

    More front end

--- frontEnd/dapp.js
+++ frontEnd/dapp.js
@@ -13,7 +13,7 @@ const contractAddress = '0xECdE515D91bb1Aa9Af274A260C15c908F4A1852d'
 // var parsed = JSON.parse(fs.readFileSync(jsonFile));
 // var contractABI = parsed.abi;
 
-import contractJSON from '../build/contracts/DonationManager.json';
+//import contractJSON from '../build/contracts/DonationManager.json';
 
 window.addEventListener('load', function() {
 
@@ -61,7 +61,7 @@ mmEnable.onclick = async () => {
   var accounts = await web3.eth.getAccounts();
   const bal = await web3.eth.getBalance(accounts[0]);
   console.log('balance = : ', await web3.eth.getBalance(accounts[0]));
-  //console.log(bal);
+  console.log(bal);
   // grab mm-current-account
   // and populate it with the current address
   var mmCurrentAccount = document.getElementById('mm-current-account');
@@ -84,7 +84,7 @@ ssSubmit.onclick = async () => {
   var web3 = await new Web3(window.ethereum)
 
   // instantiate smart contract instance
-  //var contractABI = new web3.eth.Contract(contractJSON.abi, contractAddress);
+  var contractABI = new web3.eth.Contract(contractJSON.abi, contractAddress);
 
   console.log(bal);
   console.log(contractABI);
@@ -95,7 +95,7 @@ ssSubmit.onclick = async () => {
   await dm.methods.createDonor(ssInputValue,bal).send({from: ethereum.selectedAddress})
 
 }
-
+/*
 const ssGetValue = document.getElementById('ss-get-value')
 
 ssGetValue.onclick = async () => {
@@ -114,3 +114,4 @@ ssGetValue.onclick = async () => {
   ssDisplayValue.innerHTML = 'Current Simple Storage Value: ' + value
 
 }
+*/
